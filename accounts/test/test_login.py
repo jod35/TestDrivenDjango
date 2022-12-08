dce4caac6fb58b13d009a3e7008f575b05fe6516
jod35 <jodestrevin@gmail.com>
2022-12-08 20:03:17 +0300

    user login

--- accounts/test/test_login.py
+++ accounts/test/test_login.py
@@ -1,10 +1,42 @@
 from django.test import TestCase
 from django.urls import reverse
 from http import HTTPStatus
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class LoginTest(TestCase):
+    def setUp(self):
+        self.username = "testuser123"
+        self.email ="testuser123@gmail.com"
+        self.password ="testpassword##123"
+
+        User.objects.create_user(
+            username =self.username,
+            email=self.email,
+            password=self.password
+        )
+
     def test_login_page_exists(self):
         response =self.client.get(reverse('login_page'))
 
         self.assertEqual(response.status_code,HTTPStatus.OK)
         self.assertTemplateUsed(response,'accounts/login.html')
+
+    def test_login_page_has_login_form(self):
+        response =self.client.get(reverse('login_page'))
+
+        form = response.context.get('form')
+
+        self.assertIsInstance(form,AuthenticationForm)
+    
+    def test_login_page_logs_in_user(self):
+        user_data ={
+            'username':self.username,
+            'password':self.password
+        }
+
+        response = self.client.post(reverse('login_page'),user_data)
+
+        self.assertRedirects(response,reverse('homepage'))
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegistrationForm
 from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login
 
 
 # Create your views here.
@@ -24,4 +26,23 @@ def register_user(request):
 
 
 def login_page(request):
-    return render(request,"accounts/login.html")
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+        form = AuthenticationForm(request,request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+
+                return redirect(reverse('homepage'))
+
+    context ={
+        'form':form
+    }
+    return render(request,"accounts/login.html",context)
