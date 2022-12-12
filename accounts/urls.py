04664ff4c74b8c432716476c9233ebc8cb1c29ba
jod35 <jodestrevin@gmail.com>
2022-12-12 11:51:11 +0300

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('signup/',views.register_user,name='signup_page'),
     path('login/',views.login_page,name='login_page'),
-    path('logout/',views.logout_user,name='logout')
+    path('logout/',views.logout_user,name='logout'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(
+        template_name = 'accounts/password_reset.html'
+    ),name='password_reset'),
+    path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(
+        template_name = 'accounts/password_reset_done.html'
+    ),name='password_reset_done'),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(
+        template_name = 'accounts/password_reset_confirm.html'
+    ),name='password_reset_confirm'),
+    path('password_reset_complete/',auth_views.PasswordResetCompleteView.as_view(
+        template_name = 'accounts/password_reset_complete.html'
+    ),name='password_reset_complete'),
+
 ]
--- myblog/settings.py
+++ myblog/settings.py
@@ -83,6 +83,10 @@ DATABASES = {
 }
 
 
+# EMAIL SETTINGS
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
